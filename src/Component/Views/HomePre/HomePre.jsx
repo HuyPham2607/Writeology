9c08bbbd2b0b8a04520a74754e609d0c58b739a4
HuyPham2607 <pnghuy2000@.com>
2023-04-15 15:53:56 +0700

    commit Newbranch

---
+++ src/Component/Views/HomePre/HomePre.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../../Layouts/Navbar/Navbar";
+import OnlyPre from "../../OnlyPre/OnlyPre";
+import Footer from "../../Layouts/Footer/Footer";
+
+function HomePre() {
+  return (
+    <>
+      <Navbar />
+      <OnlyPre />
+      <Footer />
+    </>
+  );
+}
+
+export default HomePre;
